37a6e30b244cfcf877774486d20a8958d57559ab
beefcake <ajkfrye@gmail.com>
2025-01-09 11:03:24 -0800

    initial edits to kickstart

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -46,3 +46,23 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- vim: ts=2 sts=2 sw=2 et
+
+-- [[ Move highlighted text with 'J' and 'K' with auto indentation ]]
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- [[ Keep cursor in place when using 'J' in normal moe to append line below to current line ]]
+vim.keymap.set('n', 'J', 'mzJ`z')
+
+-- [[ Keep cursor at mid page vertically when scrolling/searching ]]
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- [[ Preserve copied selection when paste-replacing stuff ]]
+vim.keymap.set('x', '<leader>p', [["_dP]])
+
+-- [[ add option to yank to system clipboard ]]
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
